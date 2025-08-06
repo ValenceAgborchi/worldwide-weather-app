eb61089ca6c2f12c765ad4b7b59e6dc08f7b0b9b
ValenceAgborchi <vagborchi27@gmail.com>
2025-08-06 09:28:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
 // http://api.weatherapi.com/v1/current.json?key=06e1902cbebd4f04bcf02933251605&q=Innisfil&aqi=no
 
-const temperaturefield = document.querySelector(".temp");
-const locationField = document.querySelector(".time_location p");
-const dateandTimeField = document.querySelector(".time_location span");
+const temperatureField = document.querySelector(".temp p");
+const locationField = document.querySelector(".location-name");
+const dateAndTimeField = document.querySelector(".date-time");
 const conditionField = document.querySelector(".condition p");
-const searchField = document.querySelector (".search_area");
-const form = document.querySelector('form');
+const searchField = document.querySelector(".search_area");
+const form = document.querySelector("form");
 
+form.addEventListener("submit", searchForLocation);
 
-form.addEventListener('submit' , searchForLocation);
-
-let target = 'Lucknow';
+let target = "Lucknow";
 
 const fetchResults = async (targetLocation) => {
-        let url = `http://api.weatherapi.com/v1/current.json?key=06e1902cbebd4f04bcf02933251605&q=${targetLocation}&aqi=no` ;
+    let url = `https://api.weatherapi.com/v1/current.json?key=06e1902cbebd4f04bcf02933251605&q=${targetLocation}&aqi=no`;
 
-        
+    try {
         const res = await fetch(url);
-
         const data = await res.json();
-
         console.log(data);
-
-
 
         let locationName = data.location.name;
         let time = data.location.localtime;
@@ -30,56 +25,44 @@ const fetchResults = async (targetLocation) => {
         let condition = data.current.condition.text;
 
         updateDetails(temp, locationName, time, condition);
-}
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        alert("Could not retrieve weather data. Please try again.");
+    }
+};
 
-
-function updateDetails(temp, locationName, time, condition){
-
+function updateDetails(temp, locationName, time, condition) {
     let splitDate = time.split(" ")[0];
-    
     let splitTime = time.split(" ")[1];
+    let currentDay = getDayName(new Date(splitDate).getDay());
 
-    let currentDay = getDayName(new Date(splitDate).getDay())
-
-
-
-    temperaturefield.innerText = temp;
+    temperatureField.innerText = `${temp}°C`;
     locationField.innerText = locationName;
-    dateandTimeField.innerText = `${splitDate} ${currentDay} ${splitTime}` ;
+    dateAndTimeField.innerText = `${splitDate} - ${currentDay} ${splitTime}`;
     conditionField.innerText = condition;
-
-
-
-
-}   
+}
 
 function searchForLocation(e) {
     e.preventDefault();
-
-    target = searchField.value;
-
-    fetchResults(target);
+    target = searchField.value.trim();
+    if (target) {
+        fetchResults(target);
+    }
 }
 
 fetchResults(target);
 
-function getDayName(number){
-    switch (number){
-        case 0:
-            return 'Sunday';
-        case 1:
-            return 'Monday';
-        case 2:
-            return 'Tuesday';
-        case 3:
-            return 'Wednesday';
-        case 4:
-            return 'Thursday';
-        case 5:
-            return 'Friday';
-        case 6:
-            return 'Saturday';
-                                
+function getDayName(number) {
+    switch (number) {
+        case 0: return "Sunday";
+        case 1: return "Monday";
+        case 2: return "Tuesday";
+        case 3: return "Wednesday";
+        case 4: return "Thursday";
+        case 5: return "Friday";
+        case 6: return "Saturday";
+    }
+}    
                         
 
     }
